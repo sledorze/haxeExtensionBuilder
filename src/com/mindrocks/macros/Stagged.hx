093f6ce010f210341d64283ea8bfb2dd0035e1a1
sledorze <stephane.ledorze@gmail.com>
2011-11-02 09:48:59 +0100

    implement missing cases and added some code cleaning.

--- src/com/mindrocks/macros/Stagged.hx
+++ src/com/mindrocks/macros/Stagged.hx
@@ -15,115 +15,160 @@ class Stagged {
   static function extractLookup(b : Expr) : Array<{ field : String, expr : Expr }> return {
     switch(b.expr) {
       case EObjectDecl(fields): fields;
-      default : throw ("not supported" + Std.string(b.expr));
+      default : throw ("shouldn't happened - lookup expression " + Std.string(b.expr));
     }
   }
   
   static function substitueSeveral(src : Array<Expr>, subs : Array<{field : String, expr : Expr }>) {
     for (exp in src) {
-      substitueIn(exp, subs);
+      substitueExp(exp, subs);
     }
   }
 
-  static function substitueTP(tp : TypeParam, subs : Array<{field : String, expr : Expr }>) {
+  static function substitueTypeParam(tp : TypeParam, subs : Array<{field : String, expr : Expr }>) {
     switch (tp) {
       case TPType( ct ) : substitueComplexType(ct, subs);
-	    case TPExpr( e ) : substitueIn(e, subs);
+	    case TPExpr( e ) : substitueExp(e, subs);
     }
   }
+
+  static function substitueTypePath(tp : TypePath, subs : Array<{field : String, expr : Expr }>) {
+    for (param in tp.params) {
+      substitueTypeParam(param, subs);
+    }
+  }
+
   static function substitueFunc(func : Function, subs : Array<{field : String, expr : Expr }>) {
     for (arg in func.args) {      
       substitueComplexType(arg.type, subs);
-      substitueIn(arg.value, subs);
+      substitueExp(arg.value, subs);
     }
-    substitueIn(func.expr, subs);
+    substitueExp(func.expr, subs);
     for (param in func.params) {
       for (constraint in param.constraints) {
         substitueComplexType(constraint, subs);
       }
     }
   }
-  static function substitueComplexType(func : ComplexType, subs : Array<{field : String, expr : Expr }>) {
-    throw "implement ";
+  
+  static function substitueField(field : Field, subs : Array<{field : String, expr : Expr }>) {
+    switch (field.kind) {
+      case FVar(ct, e):
+        substitueComplexType(ct, subs);
+        substitueExp(e, subs);
+      case FProp(get, set, t, e):
+        substitueComplexType(t, subs);
+        substitueExp(e, subs);
+      case FFun(f):
+        substitueExp(f.expr, subs);
+        for (funArg in f.args) {
+          substitueComplexType(funArg.type, subs);
+          substitueExp(funArg.value, subs);
+        }
+        for (param in f.params) {
+          for (constraint in param.constraints) {
+            substitueComplexType(constraint, subs);
+          }
+        }
+    }    
+  }
+
+  static function substitueComplexType(ct : ComplexType, subs : Array<{field : String, expr : Expr }>) {
+    switch (ct) {
+      case TPath(tp): substitueTypePath(tp, subs);
+      case TParent(t): substitueComplexType(t, subs);
+      case TFunction(args, ret):
+        for (arg in args) {
+          substitueComplexType(arg, subs);
+        }
+        substitueComplexType(ret, subs);
+      case TExtend(tp, fields):
+        substitueTypePath(tp, subs);
+        for (field in fields) {
+          substitueField(field, subs);
+        }
+      case TAnonymous(fields):
+        for (field in fields) {
+          substitueField(field, subs);
+        }
+    }
   }
   
-  static function substitueIn(src : Expr, subs : Array<{field : String, expr : Expr }>) {
+  static function substitueExp(src : Expr, subs : Array<{field : String, expr : Expr }>) {
     if (src == null) return;
     switch (src.expr) {
       case EConst( c ):
         switch (c) {
           case CIdent( identName ):
-//            trace("Name " + identName);
-            var found = subs.filter(function(p) return p.field == identName).first();
-            if (found != null) {
-//              trace("Substitued");
-              src.expr = found.expr.expr; // substitution occurs here.
+            for (sub in subs) {
+              if (sub.field == identName) {
+                src.expr = sub.expr.expr;
+                break;
+              }
             }
           default:
         }
-      case EArray( e1, e2) : substitueIn(e1, subs); substitueIn(e2, subs);
-      case EBinop( op, e1, e2) : substitueIn(e1, subs); substitueIn(e2, subs);
-      case EField( e, field) : substitueIn(e, subs);
-      case EType( e, field) :  substitueIn(e, subs);
-      case EParenthesis( e ) :  substitueIn(e, subs);
+      case EArray( e1, e2) : substitueExp(e1, subs); substitueExp(e2, subs);
+      case EBinop( op, e1, e2) : substitueExp(e1, subs); substitueExp(e2, subs);
+      case EField( e, field) : substitueExp(e, subs);
+      case EType( e, field) :  substitueExp(e, subs);
+      case EParenthesis( e ) :  substitueExp(e, subs);
       case EObjectDecl( fields) :
         for (field in fields)
-        substitueIn(field.expr, subs);
+        substitueExp(field.expr, subs);
       case EArrayDecl( values) : substitueSeveral(values, subs);
-      case ECall( e, params) :  substitueIn(e, subs); substitueSeveral(params, subs);
+      case ECall( e, params) :  substitueExp(e, subs); substitueSeveral(params, subs);
       case ENew( t, params) : substitueSeveral(params, subs);
-      case EUnop( op, postFix, e ) : substitueIn(e, subs); 
+      case EUnop( op, postFix, e ) : substitueExp(e, subs); 
       case EVars( vars):
         for (vr in vars) {
-          substitueIn(vr.expr, subs); 
+          substitueExp(vr.expr, subs); 
         }
       case EFunction( name, f) : substitueFunc(f, subs);        
       case EBlock( exprs) : substitueSeveral(exprs, subs);
-      case EFor( it, expr):  substitueIn(it, subs); substitueIn(expr, subs); 
-      case EIn( e1, e2) : substitueIn(e1, subs); substitueIn(e2, subs);
-      case EIf( econd, eif, eelse): substitueIn(econd, subs); substitueIn(eif, subs); substitueIn(eelse, subs);
-      case EWhile( econd, e, normalWhile): substitueIn(econd, subs); substitueIn(e, subs);
+      case EFor( it, expr):  substitueExp(it, subs); substitueExp(expr, subs); 
+      case EIn( e1, e2) : substitueExp(e1, subs); substitueExp(e2, subs);
+      case EIf( econd, eif, eelse): substitueExp(econd, subs); substitueExp(eif, subs); substitueExp(eelse, subs);
+      case EWhile( econd, e, normalWhile): substitueExp(econd, subs); substitueExp(e, subs);
       case ESwitch( e, cases, edef):
-        substitueIn(e, subs); 
+        substitueExp(e, subs); 
         for (cas in cases) {
-          substitueIn(cas.expr, subs); 
+          substitueExp(cas.expr, subs); 
         }        
-        substitueIn(edef, subs); 
+        substitueExp(edef, subs); 
       case ETry( e, catches):
-        substitueIn(e, subs); 
+        substitueExp(e, subs); 
         for (cat in catches) {
-          substitueIn(cat.expr, subs); 
+          substitueExp(cat.expr, subs); 
         }        
       case EReturn(e):
-        substitueIn(e, subs); 
+        substitueExp(e, subs); 
       case EBreak:
       case EContinue:
       case EUntyped( e):
-        substitueIn(e, subs); 
+        substitueExp(e, subs); 
       case EThrow( e):
-        substitueIn(e, subs); 
+        substitueExp(e, subs); 
       case ECast( e, t):
-        substitueIn(e, subs);
+        substitueExp(e, subs);
         substitueComplexType(t, subs);
       case EDisplay( e, isCall):
-        substitueIn(e, subs);
-      case EDisplayNew( t ): //: TypePath 
-        for (param in t.params) {
-          substitueTP(param, subs);
-        }
+        substitueExp(e, subs);
+      case EDisplayNew(t): //: TypePath 
+        substitueTypePath(t, subs);
 
       case ETernary( econd, eif, eelse):
-        substitueIn(econd, subs);
-        substitueIn(eif, subs);
-        substitueIn(eelse, subs);
+        substitueExp(econd, subs);
+        substitueExp(eif, subs);
+        substitueExp(eelse, subs);
       case ECheckType( e, t):
-        substitueIn(e, subs);
+        substitueExp(e, subs);
         substitueComplexType(t, subs);
     }
   }
   /*
   @:macro public static function subs(exp : Expr, nameToExpressions : Expr) : Expr {
-    substitueIn(exp, extractLookup(nameToExpressions));
+    substitueExp(exp, extractLookup(nameToExpressions));
     return exp;
   }
   */
@@ -151,7 +196,7 @@ class Stagged {
     for (field in Reflect.fields(mapping)) {
       arr.push( { field : field, expr : untyped Reflect.field(mapping, field) } );
     }
-    substitueIn(exp, arr);
+    substitueExp(exp, arr);
     set(exp);
     return { expr : EBlock([]), pos : exp.pos};
   }
