553f4dd21136efb199f9d6ded6e4957d45223fd7
sledorze <stephane.ledorze@gmail.com>
2011-11-02 09:22:58 +0100

    changed test

--- src/StaggedTest.hx
+++ src/StaggedTest.hx
@@ -14,6 +14,8 @@ class StaggedTest {
     var i;
     StaggedTestMacros.forExample2(i = 0, i < 10, i++, trace(i));
     
+    trace("final i " + i);
+    
     var res = StaggedTestMacros.testMeta(function (x : Int) { return x + 1; } );
     
     trace("res " + res());
