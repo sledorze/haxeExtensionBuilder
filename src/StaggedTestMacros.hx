5f2b7d9ff429d210cc1a3fcc890c5948108d34a1
sledorze <stephane.ledorze@gmail.com>
2011-11-03 23:03:02 +0100

    code clean

--- src/StaggedTestMacros.hx
+++ src/StaggedTestMacros.hx
@@ -15,46 +15,11 @@ using Lambda;
 
 class StaggedTestMacros {
 
-  /*
-  @:macro public static function testMeta(init : Expr) : Expr {
-    Stagged.setMappings({
-      init : init
-    });
-    Stagged.make(
-      function () {
-        return init(10);
-      }
-    );
-    return Stagged.get();
-  }
-
-  @:macro public static function forExample(init : Expr, cond : Expr, inc : Expr, body : Expr) : Expr {
-    Stagged.setMappings({
-      init : init,
-      cond : cond,
-      inc : inc,
-      body : body
-    });
-    Stagged.make({
-      var i;
-      init;
-      function oneTime() {
-        if (cond) {
-          body;
-          inc;
-          oneTime();
-        }
-      }
-      oneTime();
-    });
-    return Stagged.get();
-  }
-*/
   @:macro public static function forExample2(init : Expr, cond : Expr, inc : Expr, body : Expr, nb : Int = 5) : Expr return {
     
     var arr = [];
     for (ind in 0...5) {
-      var stagged =
+      arr.push(
         Stagged.stagged("{
           trace('i ' + $ind);
           $init;
@@ -66,8 +31,8 @@ class StaggedTestMacros {
             }
           }
           oneTime();
-        }");
-      arr.push(stagged);
+        }")
+      );
     }
     
     return { expr : EBlock(arr), pos : Context.currentPos() };
