9e4a7f5c3b035bc60f3c6a604ab250cfa849102a
sledorze <stephane.ledorze@gmail.com>
2011-12-30 16:13:49 +0100

    reworked Monads (more general)

--- src/MonadTest.hx
+++ src/MonadTest.hx
@@ -5,9 +5,8 @@ package ;
  * @author sledorze
  */
 
-import Prelude;
+// import Prelude;
 using PreludeExtensions;
-
 
 import com.mindrocks.functional.Functional;
 using com.mindrocks.functional.Functional;
@@ -17,27 +16,31 @@ using com.mindrocks.macros.MonadSugarMacro;
  
 class MonadTest {
 
+  public static function foo() {
+    
+  }
   
   public static function compilationTest() {
     
     var res =
-      D.o({
-        value <= Some(55);
-        value <= Some(value * 2);
-        D.o({
-          v <= Some(123);
-          Some(v + value);
+      OptionM.Do({
+        value <= ret(55);
+        value <= ret(value * 2);
+        OptionM.Do({
+          v <= ret(123);
+          ret(v + value);
         });
       }); 
     
     var res2 = 
-      D.o({
+      ArrayM.Do({
         a <= [0, 1, 2];
         b <= [10, 20, 30];
+        c <= ret(12);
         [a + b];
       });
       
     trace("result " + Std.string(res)); // MonadTest.hx:40: result Some(233)
-    trace("result2 " + Std.string(res2)); // MonadTest.hx:41: result2 [10,20,30,11,21,31,12,22,32]
+//    trace("result2 " + Std.string(res2)); // MonadTest.hx:41: result2 [10,20,30,11,21,31,12,22,32]
   }  
 }
--- src/com/mindrocks/macros/MonadSugarMacro.hx
+++ src/com/mindrocks/macros/MonadSugarMacro.hx
@@ -9,26 +9,80 @@ using com.mindrocks.macros.Staged;
 import com.mindrocks.macros.Staged;
 
 using PreludeExtensions;
-using haxe.data.collections.ArrayExtensions;
+// using haxe.data.collections.ArrayExtensions;
+
+import com.mindrocks.functional.Functional;
 
 /**
  * ...
  * @author sledorze
  */
+class OptionM {
+  @:macro public static function Do(body : Expr) return
+    Monad.Do("OptionM", body, Context)
 
-class D {
+  inline public static function ret<T>(x : T) return
+    Some(x)
+  
+  inline public static function flatMap<T>(x : Option<T>, f : T -> Option<T>) : Option<T> {
+    switch (x) {
+      case Some(x) : return f(x);
+      default : return None;
+    }
+  }
+}
 
+class ArrayM {
+  @:macro public static function Do(body : Expr) return
+    Monad.Do("ArrayM", body, Context)
 
-  @:macro public static function o(body : Expr) {
+  inline public static function ret<T>(x : T) return
+    [x]
+  
+  inline public static function flatMap<T>(xs : Array<T>, f : T -> Array<T>) : Array<T> {
+    var res = [];
+    for (x in xs) {
+      for (y in f(x)) {
+        res.push(y);  
+      }      
+    }
+    return res;
+  }  
+}
 
-    function mk(e : ExprDef) return { pos : Context.currentPos(), expr : e };
+class Monad {
 
-    function transform(e : Expr, nexts : Array<Expr>) : Array<Expr> {
+  
+  public static function Do<Q>(monadTypeName : String, body : Expr, context : Dynamic) {
+    var position : Position = context.currentPos();
+    function mk(e : ExprDef) return { pos : position, expr : e };
+
+    function promoteExpression(e : Expr) : Expr {
+      switch (e.expr) {        
+        case ECall(exp, params) :
+          switch (exp.expr) {
+            case EConst(const):
+              switch (const) {
+                case CIdent(name):
+                  try {
+                    context.typeof(exp);
+                  } catch (e : Dynamic) { // change this to a call from the Monad object.                     
+                    return mk(ECall(mk(EField(mk(EConst(CType(monadTypeName))), name)), params));
+                  }
+                default:
+              }
+            default:
+          }
+        default:
+      }
+      return e;
+    }
+    
+    function transform(e : Expr, nexts : Array<Expr>) : Array<Expr> {      
       switch (e.expr) {
-        case EBinop(op, l, r) :
+        case EBinop(op, l, rightExpr) :
           switch (op) {
-            case OpLte:
-              
+            case OpLte:              
               var name : String =
                 switch (l.expr) {
                   case EConst(c) :
@@ -39,25 +93,32 @@ class D {
                   default : null;
                 }                  
               
-              if (name != null) {              
-
-                var rest = mk(EBlock([mk(EReturn(mk(EBlock(nexts))))]));
-                var func = EFunction(null, { args : [ { name : name, type : null, opt : false, value : null } ], ret : null, expr : rest, params : [] } );
-                return [mk(ECall(mk(EField(r, "flatMap")), [mk(func)]))];
+              if (name != null) {
+                
+                var rest : Expr = mk(EReturn(mk(EBlock(nexts))));
+                var func = mk(EFunction(null, { args : [ { name : name, type : null, opt : false, value : null } ], ret : null, expr : rest, params : [] } ));
+                
+                var res = mk(ECall(mk(EField(mk(EConst(CType(monadTypeName))), "flatMap")), [promoteExpression(rightExpr), func]));
+                
+//                var res = mk(ECall(mk(EField(promoteExpression(rightExpr), "flatMap")), [func]));
+                
+                return [res];
               }
               
             default :
           }
+        
         default:
       }
-      nexts.insert(0, e);
+      nexts.insert(0, promoteExpression(e));
       return nexts; 
     }
 
     switch (body.expr) {
-      case EBlock(exprs): return mk(EBlock(exprs.foldr([], transform)));        
+      case EBlock(exprs): return mk(EBlock(exprs.foldr([], transform)));
       default : return body;
     };
   }
   
 }
+
