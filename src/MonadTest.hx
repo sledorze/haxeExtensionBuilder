5e7b1b09eb38e290f5140fbd416d8a1823104fbb
sledorze <stephane.ledorze@gmail.com>
2011-12-30 18:21:14 +0100

    Cleaner monad generation

--- src/MonadTest.hx
+++ src/MonadTest.hx
@@ -25,22 +25,23 @@ class MonadTest {
     var res =
       OptionM.Do({
         value <= ret(55);
-        value <= ret(value * 2);
-        OptionM.Do({
+        value <= ret(value * 2);        
+        x <= OptionM.Do({
           v <= ret(123);
           ret(v + value);
         });
-      }); 
-    
+        ret(x);
+      });
+      
     var res2 = 
       ArrayM.Do({
         a <= [0, 1, 2];
         b <= [10, 20, 30];
-        c <= ret(12);
-        [a + b];
+        c <= ret(1000);
+        [a + b + c];
       });
       
     trace("result " + Std.string(res)); // MonadTest.hx:40: result Some(233)
-//    trace("result2 " + Std.string(res2)); // MonadTest.hx:41: result2 [10,20,30,11,21,31,12,22,32]
+    trace("result2 " + Std.string(res2)); // MonadTest.hx:41: result2 [10,20,30,11,21,31,12,22,32]
   }  
 }
--- src/com/mindrocks/macros/MonadSugarMacro.hx
+++ src/com/mindrocks/macros/MonadSugarMacro.hx
@@ -11,6 +11,8 @@ import com.mindrocks.macros.Staged;
 using PreludeExtensions;
 // using haxe.data.collections.ArrayExtensions;
 
+// Introduire des rewrite rules (d'abord abstraire les transfos (mini AST pour separer la detection de la transformation)
+
 import com.mindrocks.functional.Functional;
 
 /**
@@ -50,9 +52,103 @@ class ArrayM {
   }  
 }
 
+enum MonadOp {
+  MExp(e : Expr);
+  MBind(e : MonadOp, bindName : String, body : MonadOp);
+  MCall(name : String, params : Array<Expr>);
+}
+
 class Monad {
 
+  public static function Do(monadTypeName : String, body : Expr, context : Dynamic) {
+    var position : Position = context.currentPos();
+    function mk(e : ExprDef) return { pos : position, expr : e };
+
+    function promoteExpression(e : Expr) : MonadOp {
+      switch (e.expr) {        
+        case ECall(exp, params) :
+          switch (exp.expr) {
+            case EConst(const):
+              switch (const) {
+                case CIdent(name):
+                  try {
+                    context.typeof(exp);
+                  } catch (e : Dynamic) {
+                    return MCall(name, params);
+                  }
+                default:
+              }
+            default:
+          }
+        default:
+      }
+      return MExp(e);
+    }
+    
+    function transform(e : Expr, nexts : Option<MonadOp>) : Option<MonadOp> {
+      switch (e.expr) {
+        case EBinop(op, l, rightExpr) :
+          switch (op) {
+            case OpLte:              
+              var name : String =
+                switch (l.expr) {
+                  case EConst(c) :
+                    switch (c) {
+                      case CIdent(name) : name;
+                      default : null;
+                    }
+                  default : null;
+                }                  
+              
+              if (name != null) {
+                var e = promoteExpression(rightExpr);
+                switch (nexts) {                  
+                  case Some(next):
+                    return Some(MBind(e, name, next));
+                  case None :
+                    return Some(e);
+                }
+              }
+              
+            default :
+          }
+        
+        default:
+      }
+      return Some(promoteExpression(e));
+    }
+    
+    function materialise(m : MonadOp) : Expr {
+      switch (m) {
+        case MExp(e) : return e;
+        case MBind(e, bindName, body) :
+          var exprs = materialise(body);
+          var rest : Expr = mk(EReturn(exprs));
+          var func = mk(EFunction(null, { args : [ { name : bindName, type : null, opt : false, value : null } ], ret : null, expr : rest, params : [] } ));
+          var res = mk(ECall(mk(EField(mk(EConst(CType(monadTypeName))), "flatMap")), [materialise(e), func]));
+          return res;
+        case MCall(name, params) :
+          var res = mk(ECall(mk(EField(mk(EConst(CType(monadTypeName))), name)), params));
+          return res;
+      }
+    }
+    
+    switch (body.expr) {
+      case EBlock(exprs):
+        switch(exprs.foldr(None, transform)) {
+          case Some(monad): return materialise(monad);
+          case None: return mk(EBlock([]));
+        }
+        
+      default : return body;
+    };
+  }
   
+}
+
+/*
+class Monad {
+
   public static function Do<Q>(monadTypeName : String, body : Expr, context : Dynamic) {
     var position : Position = context.currentPos();
     function mk(e : ExprDef) return { pos : position, expr : e };
@@ -113,7 +209,7 @@ class Monad {
       nexts.insert(0, promoteExpression(e));
       return nexts; 
     }
-
+    
     switch (body.expr) {
       case EBlock(exprs): return mk(EBlock(exprs.foldr([], transform)));
       default : return body;
@@ -121,4 +217,4 @@ class Monad {
   }
   
 }
-
+ * */
